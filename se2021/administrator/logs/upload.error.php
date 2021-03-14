772b3a630ce1f34a1a99effcd88c995c4ae15f60
Kittipong-savisai <76862484+Kittipong-savisai@users.noreply.github.com>
2021-03-14 16:48:03 +0700

    upload succesed

---
+++ se2021/administrator/logs/upload.error.php
@@ -0,0 +1,13 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-02 22:41:00 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-02T22:41:00+00:00	INFO 172.24.0.1	upload	
+2021-03-02T22:44:04+00:00	INFO 172.24.0.1	upload	
+2021-03-02T22:44:56+00:00	INFO 172.24.0.1	upload	File exists: /var/www/html/images/01.jpg by user_id 651
+2021-03-02T22:48:00+00:00	INFO 172.24.0.1	upload	
+2021-03-02T22:50:18+00:00	INFO 172.24.0.1	upload	
+2021-03-02T23:30:49+00:00	INFO 172.24.0.1	upload	
+2021-03-02T23:32:16+00:00	INFO 172.24.0.1	upload	
